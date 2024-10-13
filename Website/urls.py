628f63b70bbb6f05bf5f72964dd34f2489073255
Benjixx99 <Benjixx99@users.noreply.github.com>
2024-10-13 21:10:32 +0200

    Edit settings

--- Website/urls.py
+++ Website/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.news, name="news"),
     path("news", views.news, name="news"),
     path("news/<str:URLName>", views.newsDetail, name="news-detail"),
     path("shop", views.shop, name="shop"),
